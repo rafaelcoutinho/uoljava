09c5b05e468b7c6462dc6735c52e9edfd9b1d446
Rafael Coutinho <rafael.coutinho@gmail.com>
2018-02-10 14:17:30 -0200

    Add main class

---
+++ src/main/java/edu/inheritance/example1/Main.java
@@ -0,0 +1,13 @@
+package edu.inheritance.example1;
+
+/**
+ * Entry point for the executable Jar
+ * 
+ * @author coutinho
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
