7f7c203f49676eaceb81c06cf165105e42dc7b71
Damian Chambers <damian.chambers@yahoo.com>
2018-02-16 07:25:37 -0500

    Adding Main View to Project using Scene Builder.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 /**
  * Entry point for the executable Jar
@@ -6,8 +9,25 @@
  * @author coutinho
  *
  */
-public class Main {
+public class Main extends Application {
+	private Stage primaryStage;
+	private GridPane mainLayout;
+	
 	public static void main(String[] args) {
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage)  {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Driver App");
+		
+		showMainView();
+	}
+
+	private void showMainView() {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		
 	}
 }
